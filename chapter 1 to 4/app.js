3e156794270c1855504f3a062009892eb69d9034
Qadir39 <sa9183238@gmail.com>
2024-10-10 17:53:35 +0500

    add files and folders

---
+++ chapter 1 to 4/app.js
@@ -0,0 +1,134 @@
+// Q1
+// alert("welcome to js land");
+//Q2
+// alert("erorr! please enter a valid password");
+
+
+//Q3
+//alert("Welcome to JS Land.. \n Happy codding!");
+
+//Q4
+
+//********************chapter 2 */
+
+//Q1
+
+// var userName;
+//Q2
+
+// var myName = "Abdul Qadir";
+
+//Q3
+
+// var message ;
+// message = "hello world";
+// alert(message);
+
+//Q4
+
+// var userName = "jhon Doe";
+// var userAge = 15;
+// var eduction = "web development";
+
+// alert(userName);
+// alert(userAge);
+// alert(eduction);
+
+//Q5
+
+// var user1 = "pizz";
+// var user2 = "piz";
+// var user5 = "pi";
+// var user4 = "p";
+// alert(user1+"\n"+user2+"\n"+user5+"\n"+user4);
+
+// var user = alert("pizza\npizz\npiz\npi\np");
+
+//Q6
+
+// var email = "Qadir@gmail.com";
+// alert("My email adress is"+" "+ email);
+
+//Q7
+
+// var book = "A smarter\n way to laern javascript";
+// alert("i am trynig to learn this"+" "+ book);
+
+//Q8
+
+// document.write("yah! i can write HTML content through javascript");
+
+//Q9
+
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+// ****************CHAPTER 3*************
+
+
+//Q1
+// var age = 15;   
+// alert("I am"+ " "+age+" "+"years old");
+
+
+//Q2
+
+// var num = 13;
+// alert("you have visited this site"+" "+num+" "+"Times");
+
+//Q3
+
+// var birth = 1900;
+// document.write("My birth year is"+" " +birth+"<br> Data type of my declaerd variable is number"
+    
+// );
+
+//Q4 
+
+// var inp1 = prompt("Enter your Name");
+// var inp2 = prompt("Enter your producte title");
+// var inp3 = prompt("Enter your quantity");
+
+// document.write(inp1+" "+"ordered"+" "+inp2+" "+inp3+" "+"on Jawwad clothing store");
+
+
+
+//       ********* CHAPTER 4 ********
+
+//Q1
+
+// var a = 10;
+// var b = 20;
+// var c =  a+b;
+// document.write("you are first number is"+" "+a+" "+"and second is"+" "+b+" "+"to = "+" "+c  );
+
+//Q2
+
+ // legal variable
+// var firstNmae;
+// var firtsname;
+// var firtsname12202;
+// var firt_sname;
+// var _firtsname;
+
+// elgal variable
+
+// var 123false;
+// var fisrt@jk;
+// var first-name;
+// var @sjjdk;
+// var fisrt name;
+
+
+//Q3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+var a = "Numbers";
+var b = "$";
+var c = "_";
+var d = "Senstive";
+var e = "Keywords"
+document.write("variable name is only contain,"+" "+a+" "+b+" "+"and"+c+" "+ "for example : $my_firstvariable <br> variavle must begin with a"+" "+b+" "+"or"+" "+c+"."+"for example :$name , _name or name <br> Variable names are case"+" "+d+"<br>Variable names should not be JS"+" "+e);
+document.write()
+
