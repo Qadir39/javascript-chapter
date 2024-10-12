c44f135bf2aae4a81c928d0f41bc074cdb9dfb07
Qadir39 <sa9183238@gmail.com>
2024-10-12 19:20:45 +0500

    add flies and folders

---
+++ chapter 5/app.js
@@ -0,0 +1,228 @@
+//****************CHAPTER 5 */
+//Q1
+
+// let a = 3;
+// let b = 5;
+// let c = a+b;
+
+// document.write("Some of "+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+
+//Q2
+// let a = 3;
+// let b = 5;
+// let c = a-b;
+
+// document.write("Subtraction of "+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+
+// let a = 3;
+// let b = 5;
+// let c = a*b;
+
+// document.write("multiplication of "+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+
+// let a = 3;
+// let b = 5;
+// let c = a/b;
+
+// document.write("division of "+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+
+// let a = 30;
+// let b = 50;
+// let c = a%b;
+
+// document.write("division of "+" "+a+" "+"and"+" "+b+" "+"is"+" "+c);
+
+//Q3
+
+// var a;
+// document.write("value after variable decleartion is is"+" "+a);
+
+// a=5;
+// document.write("<br>intial value:"+"  "+a);
+// var b =++a;
+
+// document.write(" <br> value after increment is:"+ " "+a);
+
+// var add = 7;
+// var hh = a+add;
+// document.write("<br>value after addition is"+" "+hh);;
+
+// var dec = hh--;
+// document.write("<br>value after decrement is:"+" "+hh);
+
+// var bbb = hh/4-3;
+// document.write("<br>remainder is : "+" "+bbb);
+
+//Q3
+
+
+// var price = 600;
+// var tec = 5;
+// var mul =price*tec;
+
+// document.write("Total cost to buy "+" "+tec+" "+"ticket to a movie is"+" "+mul+" "+"PKR");
+
+//Q4
+
+// document.write("<h1>Table of 4</h1>");
+// var tab =4;
+// var mul =tab*1;
+// document.write("<br>"+tab+" "+" x 1 ="+" "+mul);
+// mul = tab*2;
+// document.write("<br>"+tab+" "+" x 2 ="+" "+mul);
+// mul = tab*3;
+// document.write("<br>"+tab+" "+" x 3 ="+" "+mul);
+// mul = tab*4;
+// document.write("<br>"+tab+" "+" x 4 ="+" "+mul);
+// mul = tab*5;
+// document.write("<br>"+tab+" "+" x 5 ="+" "+mul);
+// mul = tab*6;
+// document.write("<br>"+tab+" "+" x 6 ="+" "+mul);
+// mul = tab*7;
+// document.write("<br>"+tab+" "+" x 7 ="+" "+mul);
+// mul = tab*8;
+// document.write("<br>"+tab+" "+" x 8 ="+" "+mul);
+// mul = tab*9;
+// document.write("<br>"+tab+" "+" x 9 ="+" "+mul);
+// mul = tab*10;
+// document.write("<br>"+tab+" "+" x 10 ="+" "+mul);
+
+
+
+//Q5
+
+
+// let celsiusTemp = 2;
+
+
+// let fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// document.write(`${celsiusTemp}°C is ${fahrenheitTemp}°F`);
+
+
+// let fahrenheitTempInput = 86;
+
+
+// let celsiusTempConverted = (fahrenheitTempInput - 32) * 5/9;
+// document.write(`<br>${fahrenheitTempInput}°F is ${celsiusTempConverted.toFixed(2)}°C`);
+
+//  Q6
+
+
+// document.write("<h1>Shopping Cart</h1>")
+// var item1Price =650;
+// var item1Quantity=3;
+// var m1 = item1Price*item1Quantity;
+
+
+// var item2Price =100;
+// var item2Quantity=7;
+// var m2 = item2Price*item2Quantity;
+// document.write("<h1>Shopping Cart</h1>")
+// var item1Price =650;
+// var item1Quantity=3;
+// var m1 = item1Price*item1Quantity;
+//************INCOMPLETE*************** */
+
+
+
+
+
+
+//Q9
+
+// var usd =104.80;
+// var usdQuantinty =10;
+// var suadi =28;
+// var suadiQuantinty=25;
+// var usdToatl =usd*usdQuantinty;
+// var totalSuadi = suadi*suadiQuantinty;
+// var toatl =usdToatl+totalSuadi;
+// document.write("Toatal  Currency in PKR"+" "+toatl );
+// document.write(`Toatal  Currency in PKR ${toatl}` );
+
+
+
+//Q10
+
+
+// var num1 = 5+5*10/2;
+// console.log(num1);
+
+
+//Q11
+
+
+// document.write("<h1>Age Calculator</h1>")
+// var year = 2024;
+// var birth = 2004;
+// var cal = year-birth;
+// console.log(cal);
+// document.write(`Current yaer ${year} <br> Birth yaer ${birth} <br> Your Age is :${cal}`);
+
+
+//Q12
+
+
+
+
+
+// var tlp = 20;
+// var ppp = 3.142;
+// var pti = 2*ppp*tlp;
+
+// document.write(`Radius of a circle : ${tlp}`);
+// document.write(`<br>The Circumference is : ${pti}`);
+
+// var pmln = ppp*tlp*tlp;
+// document.write(`<br>The Area is : ${pmln}`);
+
+
+//Q13
+
+
+document.write("<h1>The lifetime Supply Calculator </h1>");
+
+var aa = "Chocolate Chips";
+var age = 15;
+var max = 65;
+var amount = 3;
+var yearsLeft = max - age;
+var totalSnacksNeeded = yearsLeft * 365 * amount;
+
+ document.write(`favourite snake : ${aa}<br> Current age : ${age}<br>Estimated Maximum age :${max}<br> Amount of snack per day : ${amount}<br>`);
+document.write(`You will need ${totalSnacksNeeded} ${aa} to last you until the ripe old age of ${age}.`);
+
+
+
+
+
+
+
+// Step 1: Store your favorite snack into a variable
+// const favoriteSnack = "Chips"; // Replace with your favorite snack
+
+// Step 2: Store your current age into a variable
+// const currentAge = 15; // Replace with your current age
+
+// Step 3: Store a maximum age into a variable
+// const maxAge = 65; // Replace with your desired maximum age
+
+// Step 4: Store an estimated amount per day
+// const amountPerDay = 3; // Replace with how much you eat per day
+
+// Step 5: Calculate total amount for the rest of your life
+// const yearsLeft = maxAge - currentAge;
+// const totalSnacksNeeded = yearsLeft * 365 * amountPerDay;
+
+// Output the result to the screen
+// console.log(`You will need ${totalSnacksNeeded} ${favoriteSnack} to last you until the ripe old age of ${maxAge}.`);
+
+
+
+
+
+
+
+
+
+
